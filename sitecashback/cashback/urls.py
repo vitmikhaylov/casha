0be8369f2ea759bcc35e2240a98e0bbc2cf3b43c
Vitalii Mikhaylov <kanada109@gmail.com>
2024-11-07 11:06:14 +0300

    added class CashbackHome to views.py

--- sitecashback/cashback/urls.py
+++ sitecashback/cashback/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import about, index, contact, login
+from .views import CashbackHome, about, index, contact, login
 
 urlpatterns = [
-    path('', index, name='name'),
+    path('', CashbackHome.as_view(), name='name'),
+    # path('', index, name='name'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
--- sitecashback/cashback/views.py
+++ sitecashback/cashback/views.py
@@ -1,5 +1,8 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from .models import Article
 
@@ -9,32 +12,48 @@ from .utils import menu
 def index(request):
     posts = Article.objects.all()
     data = {
-        'title': 'Home',
-        'menu': menu,
-        'posts': posts,
+        "title": "Home",
+        "menu": menu,
+        "posts": posts,
     }
-    return render(request, 'cashback/index.html', data)
+    return render(request, "cashback/index.html", data)
+
+
+class CashbackHome(ListView):
+    template_name = "cashback/index.html"
+    context_object_name = "posts"
+    extra_context = {
+        "title": "Home page",
+        "menu": menu,
+    }
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Article.objects.filter(is_published=True).select_related("category")
+
 
 def about(request):
     data = {
-        'title': 'Abaut site',
-        'menu': menu,
+        "title": "Abaut site",
+        "menu": menu,
     }
-    return render(request, 'cashback/about.html', data)
+    return render(request, "cashback/about.html", data)
+
 
 def contact(request):
     data = {
-        'title': 'Contact',
-        'menu': menu,
+        "title": "Contact",
+        "menu": menu,
     }
-    return render(request, 'cashback/contact.html', data)
+    return render(request, "cashback/contact.html", data)
+
 
 def login(request):
     data = {
-        'title': 'Login',
-        'menu': menu,
+        "title": "Login",
+        "menu": menu,
     }
-    return render(request, 'cashback/login.html', data)
+    return render(request, "cashback/login.html", data)
+
 
 def page_not_found(request, exception):
-    return HttpResponse('The page not found')
+    return HttpResponse("The page not found")
