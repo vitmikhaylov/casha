14c3eb47c751c59944ccd14371e84cf9be613e8a
Vitalii Mikhaylov <kanada109@gmail.com>
2024-11-21 18:05:06 +0300

    added context_processors.py

--- sitecashback/cashback/utils.py
+++ sitecashback/cashback/utils.py
@@ -2,7 +2,7 @@ menu = [
     {"title": "About", "url_name": "about"},
     {"title": "Feedback", "url_name": "contact"},
     # {"title": "Add page", "url_name": "add_page"},
-    {"title": "Sing in", "url_name": "login"},
+    {"title": "Sing in", "url_name": "users:login"},
 ]
 
 class DataMixin:
@@ -14,11 +14,11 @@ class DataMixin:
         if self.title_page:
             self.extra_context['title'] = self.title_page
 
-        if 'menu' not in self.extra_context:
-            self.extra_context['menu'] = menu
+        # if 'menu' not in self.extra_context:
+        #     self.extra_context['menu'] = menu
 
     def get_mixin_context(self, context, **kwargs):
-        context['menu'] = menu
+        # context['menu'] = menu
         context.update(kwargs)
         return context
 
---
+++ sitecashback/users/context_processors.py
@@ -0,0 +1,5 @@
+from cashback.utils import menu
+
+
+def get_cashback_context(request):
+    return {'mainmenu': menu}
